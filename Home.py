46493718e1380fb00b460db9ef7352d0d190cd83
ashnair5335 <109771334+ashnair5335@users.noreply.github.com>
2023-10-19 17:44:05 +0000

    Made desc, trying out columns

--- Home.py
+++ Home.py
@@ -1,3 +1,12 @@
-import streamlit
+import streamlit as st
 
-streamlit.title("Hello, world!")
+st.title("Student Schedule Builder - KUDC")
+"---"
+"Welcome to the Student Schedule Builder! Use the \
+planner below to keep your busy schedule organized \
+and do things on time!"
+"---"
+col1, col2, col3 = st.columns(3 )
+
+col1.text_area("Hello World!")
+col2.title("Hello World!")
