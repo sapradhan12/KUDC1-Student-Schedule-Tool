6198cfd28bf8ca793dc4d43d0b95f4c85e4b00c2
ashnair5335 <109771334+ashnair5335@users.noreply.github.com>
2023-10-25 08:07:14 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,7 @@
+import random
 import streamlit as st
 import sqlite3
-from streamlit_calendar import calendar
+import datetime
 
 # Create or connect to an SQLite database
 conn = sqlite3.connect('planner.db')
@@ -11,6 +12,12 @@ cursor.execute('''
     CREATE TABLE IF NOT EXISTS tasks (
         id INTEGER PRIMARY KEY,
         task TEXT
+    )
+''')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS due_date (
+        id INTEGER PRIMARY KEY,
+        due_date_input TEXT
     )
 ''')
 cursor.execute('''
@@ -31,80 +38,29 @@ conn.commit()
 st.title("High School Planner")
 st.write("Welcome to your digital planner!")
 
-# Main Planner
-st.header("Your Planner")
-
-calendar_options = {
-    "headerToolbar": {
-        "left": "today prev,next",
-        "center": "title",
-        "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
-    },
-    "slotMinTime": "06:00:00",
-    "slotMaxTime": "18:00:00",
-    "initialView": "resourceTimelineDay",
-    "resourceGroupField": "building",
-    "resources": [
-        {"id": "a", "building": "Building A", "title": "Building A"},
-        {"id": "b", "building": "Building A", "title": "Building B"},
-        {"id": "c", "building": "Building B", "title": "Building C"},
-        {"id": "d", "building": "Building B", "title": "Building D"},
-        {"id": "e", "building": "Building C", "title": "Building E"},
-        {"id": "f", "building": "Building C", "title": "Building F"},
-    ],
-}
-calendar_events = [
-    {
-        "title": "Event 1",
-        "start": "2023-07-31T08:30:00",
-        "end": "2023-07-31T10:30:00",
-        "resourceId": "a",
-    },
-    {
-        "title": "Event 2",
-        "start": "2023-07-31T07:30:00",
-        "end": "2023-07-31T10:30:00",
-        "resourceId": "b",
-    },
-    {
-        "title": "Event 3",
-        "start": "2023-07-31T10:40:00",
-        "end": "2023-07-31T12:30:00",
-        "resourceId": "a",
-    }
-]
-custom_css="""
-    .fc-event-past {
-        opacity: 0.8;
-    }
-    .fc-event-time {
-        font-style: italic;
-    }
-    .fc-event-title {
-        font-weight: 700;
-    }
-    .fc-toolbar-title {
-        font-size: 2rem;
-    }
-"""
-
-calendar = calendar(events=calendar_events, options=calendar_options, custom_css=custom_css)
-
 # To-Do List
 st.subheader("To-Do List")
 task = st.text_input("Add a new task:")
+due_date_input = st.date_input("Due date:", value=None)
 if st.button("Add Task"):
     cursor.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
+    cursor.execute("INSERT INTO due_date (due_date_input) VALUES (?)", (due_date_input,))
     conn.commit()
     st.write(f"Task added: {task}")
 
 # View all tasks
-if st.button("View All Tasks"):
-    st.subheader("All Tasks")
-    tasks = cursor.execute("SELECT task FROM tasks").fetchall()
-    for i, task in enumerate(tasks):
-        st.write(f"{i + 1}. {task[0]}")
+#if st.button("View All Tasks"):
+st.subheader("All Tasks")
+tasks = cursor.execute("SELECT task FROM tasks").fetchall()
+due_dates = cursor.execute("SELECT due_date_input FROM due_date").fetchall()
+for i, task, due_date_input in range(enumerate(enumerate(tasks), enumerate(due_dates))):
+    st.write(f"{i + 1}. {int(task)}, due {int(due_date_input)}.")
 
+'''
+for i, due_date_input in enumerate(due_dates):
+    st.write(f"Due {due_date_input}.")
+    st.checkbox("Done?", key=i)
+'''
 # Notes and Resources
 st.subheader("Notes and Resources")
 note = st.text_area("Add a note or resource:")
@@ -117,6 +73,7 @@ if st.button("Save Note"):
 if st.button("View All Notes"):
     st.subheader("All Notes")
     notes = cursor.execute("SELECT note FROM notes").fetchall()
+    due_date_db = cursor.execute("SELECT ")
     for i, note in enumerate(notes):
         st.write(f"{i + 1}. {note[0]}")
 
